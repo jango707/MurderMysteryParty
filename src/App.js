6a99ac521261b8103aecdc207d21d196ab4a5ebf
Jang <jangbel99@gmail.com>
2022-01-12 14:26:28 +0000

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacta
-        </a>
-      </header>
+      <div>
+        <h1>
+          Welcome to JLau's Murder Mystery Party
+        </h1>
+        <h2>
+          Please put your passcode to see your role
+        </h2>
+
+        <input>
+        </input>
+
+        <button>
+          Go
+        </button>
+
+      </div>
     </div>
   );
 }
